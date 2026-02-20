8d7249e7eee9f5f82aea5f299933939365d500fa
33arc <quickselect@proton.me>
2026-02-20 12:51:49 +0300

    feat: implement DNS,TCP, and HTTP/HTTPS analysis

--- src/homework/main.py
+++ src/homework/main.py
@@ -1,6 +1,60 @@
 import sys
 from .reader import load_pcap
-from .summary import summarize_packets
+from .summary import summarize_packets,dns_analysis,tcp_analysis,http_https_analysis
+
+def display_dns_report(report):
+    print("\n===================DNS REPORT===================")
+    print(f"\nDuring capture, {report['total_queries']} DNS query packets were observed")
+    print(f"{len(report["unique_domains"])} unique domains were queried.")
+    for domain,ips in report["resolutions"].items():
+        if ips:
+            print(f"The domain {domain} was resolved to:")
+            for ip in sorted(ips):
+                print(f"    -   {ip}")
+            print()
+    if report["protocols"]:
+        print("DNS operates over UDP port 53.")
+
+def display_tcp_report(report):
+    print("\n===================TCP REPORT===================")
+    if report["completed_handshakes"]:
+        for i in report['completed_handshakes']:
+            client_ip = i[0]
+            server_ip = i[1]
+            print(f"A TCP three-way handshake was observed between client "
+                  f"({client_ip}) and server ({server_ip}).")
+    else:
+        print("No complete TCP three-way handshake was observed.")
+    print(f"\nTotal TCP connections observed: {report['total_connections']}")
+
+def display_web_report(web_report):
+    print("\n===================HTTP/HTTPS REPORT===================")
+    # http
+    if web_report['http_requests']:
+        print("HTTP traffic was observed.\n")
+        for req in web_report['http_requests']:
+            print(f'GET request detected for host: {req['host']}')
+        for res in web_report['http_responses']:
+            print(f'Response Code: {res['status']}')
+            print(f'Content-Type: {res['content_type']}')
+    else:
+        print("No HTTP traffic detected.")
+    # https
+    if web_report['tls_info']:
+        print('HTTPS (TLS) traffic was observed.\n')
+        for entry in web_report['tls_info']:
+            if entry['type']=='Client Hello':
+                print('TLS Client Hello detected.')
+                print(f'TLS Version: {entry['version']}')
+            elif entry['type']=='Server Hello':
+                print('TLS Server Hello detected.')
+                print(f'TLS Version: {entry['version']}')
+                print(f'Cipher Suite: {entry['cipher']}')
+        print('\nImportant Point:')
+        print('HTTPS is encrypted. Page content cannot be viewed only handshake details.')
+    else:
+        print("No HTTPS traffic detected.")
+
 
 def main():
     if len(sys.argv) != 2:
@@ -11,8 +65,15 @@ def main():
     packets = load_pcap(path)
 
     # 1. DNS analysis
+    dns_report = dns_analysis(packets)
+    display_dns_report(dns_report)
     # 2. TCP analysis
+    tcp_report = tcp_analysis(packets)
+    display_tcp_report(tcp_report)
     # 3. HTTP/HTTPS analysis
+    web_report = http_https_analysis(packets)
+    display_web_report(web_report)
+    # 4. final
     result = summarize_packets(packets)
 
     print("Total packets:", result["total_packets"])
--- src/homework/summary.py
+++ src/homework/summary.py
@@ -1,4 +1,7 @@
-from collections import Counter
+from collections import Counter,defaultdict
+from scapy.all import DNS,DNSQR,DNSRR,IP,UDP,TCP
+from scapy.layers.http import HTTPRequest,HTTPResponse
+from scapy.layers.tls.all import TLSClientHello, TLSServerHello
 
 def summarize_packets(packets):
     total = len(packets)
@@ -15,3 +18,116 @@ def summarize_packets(packets):
         "total_packets": total,
         "protocol_counts":dict(protocols),
     }
+
+def dns_analysis(packets):
+    dns_queries=0
+    domains=set()
+    resolutions=defaultdict(set)
+    protocols=set()
+    ports=set()
+
+    for pkt in packets:
+        if pkt.haslayer(DNS) and pkt.haslayer(IP):
+            dns=pkt[DNS]
+            # count queries
+            if dns.qr == 0 and dns.qd:
+                dns_queries+=1
+                domain=dns.qd.qname.decode(errors="ignore").rstrip(".")
+                domains.add(domain)
+            # extract responses (A records only)
+            if dns.qr == 1 and dns.an:
+                for i in range(dns.ancount):
+                    ans=dns.an[i]
+                    if isinstance(ans,DNSRR) and ans.type==1: # A record
+                        domain=ans.rrname.decode(errors="ignore").rstrip(".")
+                        resolutions[domain].add(ans.rdata)
+            # transport info
+            if pkt.haslayer(UDP):
+                protocols.add("UDP")
+                ports.add(pkt[UDP].dport)
+
+    return {
+        "total_queries":dns_queries,
+        "unique_domains":domains,
+        "resolutions":resolutions,
+        "protocols":protocols,
+        "ports":ports
+    }
+
+def tcp_analysis(packets):
+    connections = defaultdict(dict)
+    completed_handshakes=[]
+
+    for pkt in packets:
+        if pkt.haslayer(TCP) and pkt.haslayer(IP):
+            ip=pkt[IP]
+            tcp=pkt[TCP]
+
+            src=ip.src
+            dst=ip.dst
+            sport=tcp.sport
+            dport=tcp.dport
+
+            connection_key=(src,dst,sport,dport)
+
+            if tcp.flags=="S": # SYN
+                connections[connection_key]["SYN"]=True
+            elif tcp.flags=="SA": # SYN-ACK
+                reverse_key=(dst,src,dport,sport)
+                connections[reverse_key]["SYN-ACK"]=True
+            elif tcp.flags=="A": # ACK
+                if "SYN" in connections[connection_key] and \
+                        "SYN-ACK" in connections[connection_key]:
+                            connections[connection_key]["ACK"]=True
+                            # Handshake completed
+                            completed_handshakes.append(connection_key)
+    total_connections = len(connections)
+    return {
+        "total_connections":total_connections,
+        "completed_handshakes":completed_handshakes
+    }
+
+def http_https_analysis(packets):
+    http_requests=[]
+    http_responses=[]
+    tls_info=[]
+
+    for pkt in packets:
+        # for http
+        if pkt.haslayer(HTTPRequest):
+            http=pkt[HTTPRequest]
+            host=http.Host.decode() if http.Host else "Unknown"
+            method = http.Method.decode()
+            http_requests.append({
+                "method":method,
+                "host":host
+            })
+        if pkt.haslayer(HTTPResponse):
+            response=pkt[HTTPResponse]
+            code=response.Status_Code.decode()
+            content_type=response.Content_Type.decode() if response.Content_Type else "Unknown"
+            http_responses.append({
+                "status":code,
+                "content_type":content_type
+            })
+        # TLS/HTTP
+        if pkt.haslayer(TLSClientHello):
+            ch=pkt[TLSClientHello]
+            tls_info.append({
+                "type":"Client Hello",
+                "version":ch.version
+            })
+        if pkt.haslayer(TLSServerHello):
+            sh=pkt[TLSServerHello]
+            tls_info.append({
+                "type":"Server Hello",
+                "version":sh.version,
+                "cipher":sh.cipher
+            })
+
+    return {
+        "http_requests":http_requests,
+        "http_responses":http_responses,
+        "tls_info":tls_info
+    }
+
